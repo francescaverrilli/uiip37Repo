823ad05830bc76d5c860e65cf071b775e9616e2b
AndreaGuastaferro <andrea.guastaferro91@gmail.com>
2018-10-15 22:16:35 +0200

    Ho creato la classe Andrea

---
+++ JavaProject1/src/eser1/Andrea.java
@@ -0,0 +1,11 @@
+package eser1;
+
+public class Andrea {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Il mio nome è Andrea");
+		System.out.println("Il mio cognome è Guastaferro");
+	}
+
+}
