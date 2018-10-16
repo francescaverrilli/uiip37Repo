0f1f6efe9103bfa594954951de9626968ece106f
francescoabbatangelo <francescoabbatangelo91@gmail.com>
2018-10-16 17:28:20 +0200

    Studente

--- Esercitazione_Gruppo_1/src/eser1/Client.java
+++ Esercitazione_Gruppo_1/src/eser1/Client.java
@@ -7,55 +7,56 @@ import java.util.Vector;
 public class Client {
 
 	public static void main(String[] args) {
-		String corso = "elettrotecnica";
-
+		// Creazione Persone
 		Indirizzo ind1 = new Indirizzo("Via del Tintoretto", 28, "Modena", 41100, "MO", "Emilia Romagna");
 		Persona ema = new Persona("Emanuele", "Avallone", "18/05/1992", ind1);
 		System.out.println(ema);
-		// String via, int civico, String città, int cAP, String provincia, String
-		// regione
+
 		Indirizzo myIndirizzo = new Indirizzo("C/da Pino", 9, "Benevento", 82110, "BN", "Campania");
-		// String nome, String cognome, String datanascita, String indirizzo
 		Persona io = new Persona("Silvio", "Barbato", "06/09/1991", myIndirizzo);
-		Studente Silvio = new Studente(io.getNome(), io.getCognome(), io.getDatanascita(), io.getIndirizzo(), 1,
-				"Ingegneria Energetica");
+		System.out.println(io);
 
-		System.out.print(io);
-
-		System.out.print("\n");
 		Indirizzo indirizzo = new Indirizzo("C/DA Difesa Grande", 52, "Ariano Irpino", 83031, "AV", "Campania");
 		Persona frank = new Persona("Francesco", "Abbatangelo", "10/05/1991", indirizzo);
 		System.out.print(frank);
 
+		// <------------------------------------------------------------------->
+		// Ricerca residenti Silvio
 		Vector<Persona> p = new Vector<Persona>();
 		p.add(io);
 		p.add(ema);
 		p.add(frank);
-
 		System.out.print("\n" + ResidentiCampaniaSilvio.trovaResidentiCampania(p));
 
+		// <------------------------------------------------------------------->
+		// Ricerca residenti Francesco
 		System.out.print("\n\n\n");
 		Residenti_Campania_Frank elenco = new Residenti_Campania_Frank();
 		elenco.aggiungiPersona(ema);
 		elenco.aggiungiPersona(io);
 		elenco.aggiungiPersona(frank);
-		LinkedList<Persona> citt = new LinkedList<Persona>();
+		System.out.println(elenco.residentiCampaniaFrank());
 
+		// <------------------------------------------------------------------->
+		// Ricerca residenti Emanuele
+		LinkedList<Persona> citt = new LinkedList<Persona>();
 		citt.add(io);
 		citt.add(ema);
 		citt.add(frank);
-
 		ResidentiCampaniaEma res = new ResidentiCampaniaEma(citt);
 		LinkedList<Persona> campani = res.trovapersone();
 		System.out.println(campani);
 
-		System.out.println(elenco.residentiCampaniaFrank());
 		// <------------------------------------------------------------------------------------------>
-
+	
+		// Ricerca Studenti Silvio
+		Studente Silvio = new Studente(io.getNome(), io.getCognome(), io.getDatanascita(), io.getIndirizzo(), 1,
+				"Ingegneria Energetica");
 		String corsoSilvio = "UIIP";
 		Silvio.aggiungiCorso(corsoSilvio);
 		System.out.print(Silvio);
 
+		// Ricerca Studenti Silvio
 		Studente eman = new Studente("Emanuele", "Avallone", "18/05/1992", ind1, 1, "Ingegneria Biomedica");
 
 		Studente Abba = new Studente(frank.getNome(), frank.getCognome(), frank.getDatanascita(), frank.getIndirizzo(),
