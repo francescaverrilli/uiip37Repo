ecd74217c44e591904a8c681326682fd279cf0ee
AngelaDiPietro <angeladipietro92@gmail.com>
2018-10-16 18:23:27 +0200

    main prodotto

--- Progetto_Gruppo2b/src/Main.java
+++ Progetto_Gruppo2b/src/Main.java
@@ -4,6 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		//Cliente(String codiceCartaFidelity, String nome, String cognome, int numeroPunti)
+		Cliente Angela=new Cliente ("adpxyeryu","Angela","Di Pietro",89);
+		
+		//Prodotto(int codProdotto, String descrizione, double costo, int numPunti)
+		Prodotto patate=new Prodotto (34,"frutta e verdura",0.89,1);
+		Prodotto insalata=new Prodotto (68,"frutta e verdura",0.50,1);
 	}
 
 }
