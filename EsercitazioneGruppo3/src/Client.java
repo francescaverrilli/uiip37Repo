e428e2a59f893871e5153756682a9fbb9636124e
AndreaGuastaferro <andrea.guastaferro91@gmail.com>
2018-10-16 14:39:15 +0200

    Classe Client.java

--- EsercitazioneGruppo3/src/Client.java
+++ EsercitazioneGruppo3/src/Client.java
@@ -3,7 +3,12 @@ public class Client {
 	
 	public static void main(String[] args) {
 		
-		//Persona p = new Persona("")
+		Indirizzo indirizzo= new Indirizzo("Via f. Tedesco",39,"Taurasi","83030","Avellino","Campania");
+		Persona p = new Persona("Andrea","Guastaferro","24/09/91",indirizzo);
+		
+		System.out.println(p.toString());
+		
+		
 	}
 
 }
