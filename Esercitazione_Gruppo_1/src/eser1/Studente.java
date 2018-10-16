07ee351db4d2a316dcfe99ec823908128b9042d7
francescoabbatangelo <francescoabbatangelo91@gmail.com>
2018-10-16 16:21:11 +0200

    Studente

---
+++ Esercitazione_Gruppo_1/src/eser1/Studente.java
@@ -0,0 +1,47 @@
+package eser1;
+
+import java.util.LinkedList;
+
+public class Studente extends Persona {
+	private int livello;
+	private String titolo;
+	private LinkedList<String> corsi;
+
+	public Studente(String nome, String cognome, String datanascita, Indirizzo indirizzo, int livello, String titolo) {
+		super(nome, cognome, datanascita, indirizzo);
+		this.livello = livello;
+		this.titolo = titolo;
+		this.corsi = new LinkedList<String>();
+
+	}
+
+	public int getLivello() {
+		return livello;
+	}
+
+	public void setLivello(int livello) {
+		this.livello = livello;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LinkedList<String> getCorsi() {
+		return corsi;
+	}
+
+	public void setCorsi(LinkedList<String> corsi) {
+		this.corsi = corsi;
+	}
+
+	@Override
+	public String toString() {
+		return "Studente [livello=" + livello + ", titolo=" + titolo + ", corsi=" + corsi + "]";
+	}
+
+}
