98ead1f32d47aef66c492b0e727c38bc24a9a27a
emanueleavallone1992 <soprasteria@ITEM-S64618.emea.msad.sopra>
2018-10-16 18:20:28 +0200

    emadata

--- Esercitazione2_Gruppo1A/src/Client.java
+++ Esercitazione2_Gruppo1A/src/Client.java
@@ -5,5 +5,7 @@ public class Client {
 		
 
 	}
-
+	Cliente cl1= new Cliente("TREYUO", "Emanuele", "Avallone", 250);
+	Prodotto p1= new Prodotto("TTRREE", "Pan Bauletto", 1.30f, 5);
+	Prodotto p2= new Prodotto("UUGGFF", "Nutella", 3.50f, 10);
 }
