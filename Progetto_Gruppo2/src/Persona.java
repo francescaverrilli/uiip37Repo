03ec3f97f685fe7343eec5bddf5817fab001200e
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-16 14:27:10 +0200

    commit classe persona

---
+++ Progetto_Gruppo2/src/Persona.java
@@ -0,0 +1,52 @@
+
+public class Persona {
+
+	private String nome;
+	private String cognome;
+	private String dataNascita;
+	private String indirizzo;
+	public Persona(String nome, String cognome, String dataNascita, String indirizzo) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.indirizzo = indirizzo;
+	}
+	
+	private Persona() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	
+	
+	
+	
+}
