0902603d3e4e1be2bbb4e286484a200b8f39cca7
VincenzoCaserta <caserta.vincenzo@yahoo.it>
2018-10-16 15:14:38 +0200

    Creata classe ResidentiCampania

---
+++ EsercitazioneGruppo3/src/ResidentiCampaniaVIncenzo.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class ResidentiCampaniaVIncenzo {
+	
+	public static void stampaResidentiCampania(Vector<Persona> persone) {
+		
+		for(Persona p: persone) {
+			if(p.getNuovo_indirizzo().getRegione().equalsIgnoreCase("Campania")) {
+				System.out.println(p);
+			}	
+		}
+	}
+}
