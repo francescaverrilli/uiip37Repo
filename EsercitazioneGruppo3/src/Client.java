311aa041542ab4aadaa5dc6ac17f09a0eb0f1a7a
VincenzoCaserta <caserta.vincenzo@yahoo.it>
2018-10-16 14:40:37 +0200

    Client

--- EsercitazioneGruppo3/src/Client.java
+++ EsercitazioneGruppo3/src/Client.java
@@ -3,7 +3,11 @@ public class Client {
 	
 	public static void main(String[] args) {
 		
-		//Persona p = new Persona("")
+		Persona p = new Persona("vincenzo", "caserta", "27/11/1991",
+				new Indirizzo("Via cesinola", 2, "S. Martino V.C.", "83018", "AV", "Campania"));
+		
+		
+		System.out.println(p);
 	}
 
 }
