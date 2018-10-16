82bc388eee76a40d751bd66663a1b08800aaa6f9
AndreaGuastaferro <andrea.guastaferro91@gmail.com>
2018-10-16 18:17:42 +0200

    Classe cliente

---
+++ Esercitazione2GruppoUltimaFila/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Prodotto nuovo_prodotto= new Prodotto("0985", "Alimento", 2.30, 3);
+		Prodotto nuovo_prodotto1= new Prodotto("0987", "Frutta", 6.70, 2);
+		
+		Cliente cliente1= new Cliente("0374", "Andrea", "Guastaferro", 320);
+		
+		
+	}
+
+}
