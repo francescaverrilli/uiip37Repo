ae77baca8face55b4b0cd084b717aa90c35da3b0
silviobarbato <silviobarbato@live.it>
2018-10-16 18:20:42 +0200

    Cliente e prodotti silvio

--- Esercitazione2_Gruppo1A/src/Client.java
+++ Esercitazione2_Gruppo1A/src/Client.java
@@ -3,6 +3,15 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		//(String codice, String nome, String cognome, int punti)
+		Cliente silvio= new Cliente("ID1234", "Silvio", "Barbato", 10);
+		
+		
+		//String codice, String descrizione, float costo, int npunti
+		Prodotto prodottoSilvio1= new Prodotto("ID9876", "PC", 349.99f, 5 );
+		Prodotto prodottoSilvio2= new Prodotto("ID9877", "forno", 249.99f, 3 );
+		
+		
 
 	}
 
