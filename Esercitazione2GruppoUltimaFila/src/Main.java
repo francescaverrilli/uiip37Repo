8b2d7ab3b180f1a0a17c9bfdfda9055b18e2f6b7
CarloDeGennaro <carlodegennaro1@gmail.com>
2018-10-16 18:21:24 +0200

    mainCarlo

---
+++ Esercitazione2GruppoUltimaFila/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente c1 = new Cliente ("0987cd", "Carlo", "De Gennaro", 500);
+		Prodotto p1 = new Prodotto ("P123", "yogurt", 1.69, 10);
+		Prodotto p2 = new Prodotto ("P987", "uva", 3.49, 20);
+	}
+
+}
