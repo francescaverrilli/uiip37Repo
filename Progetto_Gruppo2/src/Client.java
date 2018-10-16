84185e0142b943326ebb810d4f8e599e7a96318b
AngelaDiPietro <angeladipietro92@gmail.com>
2018-10-16 14:51:01 +0200

    Angela

--- Progetto_Gruppo2/src/Client.java
+++ Progetto_Gruppo2/src/Client.java
@@ -5,9 +5,14 @@ public class Client {
 		
 		
 		
-		//Persona p_Daniela= new Persona("Daniela", "Guardabascio", "19 settembre 1985", "Via Monticchio 4/B");
+		Indirizzo i= new Indirizzo (34, "Via Iscalonga",70,83036,"Mirabella Eclano","AV","Campania");
+		Persona Angela= new Persona("Angela", "Di Pietro", "11/06/1992",i);
 		
-
+		System.out.println("Il nome della Persona è "+Angela.getNome());
+		System.out.println("Il cognome"+Angela.getCognome());
+		System.out.println("Nata il "+Angela.getDataNascita());
+		System.out.println("Residente a"+i.getCitta()+i.getNumerocivvico()+i.getProvincia()+i.getRegione());
+		
 	}
 
 }
