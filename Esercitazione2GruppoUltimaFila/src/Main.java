0b79d6f9ca9a226e56cabcc7024287feb56f3d46
VincenzoCaserta <caserta.vincenzo@yahoo.it>
2018-10-16 18:22:57 +0200

    Main Vin

---
+++ Esercitazione2GruppoUltimaFila/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Prodotto pro1 = new Prodotto("A5", "Salsiccia", 2.0, 10);
+		Prodotto pro2 = new Prodotto("A6", "Pane", 3.1, 20);
+		
+		Cliente vin = new Cliente("CSR", "Vincenzo", "Caserta", 0);
+
+	}
+
+}
