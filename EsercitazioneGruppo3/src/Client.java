fff3c40dc17aee80f32e740aee3b4b590175b05e
CarloDeGennaro <carlodegennaro1@gmail.com>
2018-10-16 14:45:31 +0200

    merge

--- EsercitazioneGruppo3/src/Client.java
+++ EsercitazioneGruppo3/src/Client.java
@@ -3,7 +3,10 @@ public class Client {
 	
 	public static void main(String[] args) {
 		
-		//Persona p = new Persona("")
+		Indirizzo i = new Indirizzo ("via Galatina", 50, "Santa Maria Capua Vetere", "81055", "CE", "Campania");
+		
+		Persona p = new Persona("Carlo", "De Gennaro", "08/08/1985", i);
+		System.out.println(p);
 	}
 
 }
