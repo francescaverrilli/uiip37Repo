2c25d917026aa297aa9a142171076fe75db68276
chiaraannicchiarico <chiara.annicchiaricopetruzzelli@gmail.com>
2018-10-16 17:08:16 +0200

    CorsoFormazione

--- Progetto_Gruppo2/src/CorsoFormazioneAnnicchiarico.java
+++ Progetto_Gruppo2/src/CorsoFormazioneAnnicchiarico.java
@@ -1,13 +1,15 @@
 import java.util.LinkedList;
 
 public class CorsoFormazioneAnnicchiarico {
-public static LinkedList<Studente> corsiFormazione (LinkedList<Studente> elencoCorsi){
-	LinkedList<Studente> listaCorsi= new LinkedList<Studente>();
-	for(Studente s : elencoCorsi ) {
-		if(s.getCorsiFormazione().equals(listaCorsi)) {
-			listaCorsi.add(s);
+	public static void StampaStudenti(LinkedList<Studente> l) {
+		for(Studente s:l) {
+			if(s.getCorsiFormazione().size()>0) {
+			System.out.println(s.getNome());
+			System.out.println(s.getCognome());
+			System.out.println(s.getTitoloStudio());
+			System.out.println(s.getCorsiFormazione());
+			}
 		}
 	}
-	return listaCorsi;
-}
+
 }
