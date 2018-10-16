e02c889d74fb891e440ba9fa1b12e86257763db8
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-16 15:46:22 +0200

    Ho aggiustato il codice

--- Progetto_Gruppo2/src/Client.java
+++ Progetto_Gruppo2/src/Client.java
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 
-import java.util.LinkedList;
-
-
-=======
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 7136990fc9b4cf2d710da786c94cfa5709f33c73
->>>>>>> 9fa7ab599054c10a581bedc3c55a79561586792c
 import java.util.*;
 
 public class Client {
@@ -21,13 +10,16 @@ public class Client {
 		
 		Indirizzo ind = new Indirizzo(10, "via bari", 28, 72013, "Ceglie Messapica", "br", "puglia");
 		Persona p = new Persona("sergio", "santoro", "21/12/1991", ind);
+		System.out.println(" ");
 		System.out.println(p.getNome());
 		System.out.println(p.getCognome());
 		System.out.println(p.getDataNascita());
 		System.out.println(p.getIndirizzo().getVia());
 		System.out.println(p.getIndirizzo().getNumerocivvico());
 		
+		
 		elencoPersone.add(p);
+		
 		Indirizzo i_Daniela = new Indirizzo(10, "Via Monticchio", 4, 83031, "Ariano Irpino", "AV", "Campania");
 		Persona p_Daniela = new Persona("Daniela", "Guardabascio", "19 settembre 1985", i_Daniela);
 		System.out.println("Dati Anagrafici");
@@ -35,8 +27,6 @@ public class Client {
 		System.out.println(p_Daniela.getCognome());
 		System.out.println(p_Daniela.getDataNascita());
 		System.out.println("Indirizzo:");
-		
-
 		elencoPersone.add(p_Daniela);
 
 		Indirizzo i = new Indirizzo(34, "Via Iscalonga", 70, 83036, "Mirabella Eclano", "AV", "Campania");
@@ -53,12 +43,9 @@ public class Client {
 		
 
 		elencoPersone.add(Angela);
-<<<<<<< HEAD
-	Indirizzo indirizzo = new Indirizzo (12, "Contrada San Cesareo", 30, 83030, "Zungoli", "AV", "Campania");
-=======
-
+		System.out.println(" ");
+		
 		Indirizzo indirizzo = new Indirizzo (12, "Contrada San Cesareo", 30, 83030, "Zungoli", "AV", "Campania");
->>>>>>> 9fa7ab599054c10a581bedc3c55a79561586792c
 		Persona Chiara = new Persona ("Chiara", "Annicchiarico", "16/04/90", indirizzo);
 		System.out.println("Dati Anagrafici");
 		System.out.println(Chiara.getNome());
@@ -68,17 +55,9 @@ public class Client {
 		System.out.println(Chiara.getIndirizzo().getVia() + " " + Chiara.getIndirizzo().getNumerocivvico() + " "
 				+ Chiara.getIndirizzo().getCitta());
 
-
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 7136990fc9b4cf2d710da786c94cfa5709f33c73
->>>>>>> 9fa7ab599054c10a581bedc3c55a79561586792c
 		elencoPersone.add(Chiara);
-		
+		System.out.println(" ");
+		System.out.println("Stampa Daniela ");
 		ResidentiCampaniaGuardabascio.residentiCampania(elencoPersone);
 		System.out.println("Elenco persone residenti in campania");
 		for( Persona p_d : elencoPersone) {
@@ -86,7 +65,8 @@ public class Client {
 		}
 		
 
-				
+		System.out.println(" ");
+		System.out.println("Stampa Sergio ");
 		ArrayList<Persona> arr = new ArrayList<Persona>();
 		arr.add(p);
 		arr.add(p_Daniela);
@@ -95,29 +75,25 @@ public class Client {
 		
 		ResidentiCampaniaSantoro.Stampa(arr);
 		
-<<<<<<< HEAD
-		
+		System.out.println(" ");
+		System.out.println("Stampa Angela ");
 		ArrayList<Persona> array = new ArrayList<Persona>();
-		arr.add(Angela);
-		arr.add(p_Daniela);
-		arr.add(Chiara);
-		arr.add(p);
+		array.add(Angela);
+		array.add(p_Daniela);
+		array.add(Chiara);
+		array.add(p);
 		ResidentiCampaniaDiPietro.Stampa(array);
-		
-<<<<<<< HEAD
+
+		System.out.println(" ");
+		System.out.println("Stampa Chiara ");
 		elencoPersone.add(Chiara);
 		ArrayList<Persona> array1 = new ArrayList<Persona>();
 		array1.add(p);
 		array1.add(p_Daniela);
 		array1.add(Chiara);
 		array1.add(Angela);
+		ResidentiCampaniaAnnicchiarico.Stampa(array1);
 
-=======
-		
-		
-=======
->>>>>>> 7136990fc9b4cf2d710da786c94cfa5709f33c73
->>>>>>> 9fa7ab599054c10a581bedc3c55a79561586792c
 	}
 
 }
