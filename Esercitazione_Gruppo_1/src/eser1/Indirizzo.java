2ff04351935b683651caf5a2c0f532449d192bf8
silviobarbato <silviobarbato@live.it>
2018-10-16 14:25:19 +0200

    Classe Indirizzo

---
+++ Esercitazione_Gruppo_1/src/eser1/Indirizzo.java
@@ -0,0 +1,109 @@
+package eser1;
+
+public class Indirizzo {
+
+	private String via;
+	private int civico;
+	private String città;
+	private int CAP;
+	private String provincia;
+	private String regione;
+	
+	
+	public Indirizzo() {
+		
+	}
+	
+	
+	public Indirizzo(String via, int civico, String città, int cAP, String provincia, String regione) {
+		super();
+		this.via = via;
+		this.civico = civico;
+		this.città = città;
+		CAP = cAP;
+		this.provincia = provincia;
+		this.regione = regione;
+	}
+
+
+
+	public String getVia() {
+		return via;
+	}
+
+
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+
+
+	public int getCivico() {
+		return civico;
+	}
+
+
+
+	public void setCivico(int civico) {
+		this.civico = civico;
+	}
+
+
+
+	public String getCittà() {
+		return città;
+	}
+
+
+
+	public void setCittà(String città) {
+		this.città = città;
+	}
+
+
+
+	public int getCAP() {
+		return CAP;
+	}
+
+
+
+	public void setCAP(int cAP) {
+		CAP = cAP;
+	}
+
+
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+
+
+	public String getRegione() {
+		return regione;
+	}
+
+
+
+	public void setRegione(String regione) {
+		this.regione = regione;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Indirizzo [via=" + via + ", civico=" + civico + ", città=" + città + ", CAP=" + CAP + ", provincia="
+				+ provincia + ", regione=" + regione + "]";
+	}
+	
+	
+}
