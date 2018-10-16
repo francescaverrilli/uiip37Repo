5a36674b1867d3a820f5705f1893af6ef362d624
chiaraannicchiarico <chiara.annicchiaricopetruzzelli@gmail.com>
2018-10-16 18:16:34 +0200

    Nuovo cliente e prodotti

--- Progetto_Gruppo2b/src/Main.java
+++ Progetto_Gruppo2b/src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cliente c1 = new Cliente ("c123", "Chiara", "Annicchiarico", 230 );
+		
+		Prodotto p1 = new Prodotto (54, "prodotto1", 23, 25);
+		Prodotto p2 = new Prodotto (37, "prodotto2", 12, 11);
 	}
 
 }
