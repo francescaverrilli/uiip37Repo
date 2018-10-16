50974e2e685841cf271580233485a209e5c105d2
francescoabbatangelo <francescoabbatangelo91@gmail.com>
2018-10-16 18:13:39 +0200

    Frank

---
+++ Esercitazione2_Gruppo1A/src/Scontrino.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+
+public class Scontrino {
+	private String codiceScontrino;
+	private Cliente cliente;
+	private LinkedList<Prodotto> lista;
+	private double importo;
+
+	public Scontrino() {
+		super();
+		this.codiceScontrino = "";
+		this.cliente = null;
+		this.lista = new LinkedList<Prodotto>();
+		this.importo = 0;
+	}
+
+	public String getCodiceScontrino() {
+		return codiceScontrino;
+	}
+
+	public void setCodiceScontrino(String codiceScontrino) {
+		this.codiceScontrino = codiceScontrino;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public LinkedList<Prodotto> getLista() {
+		return lista;
+	}
+
+	public void setLista(LinkedList<Prodotto> lista) {
+		this.lista = lista;
+	}
+
+	public double getImporto() {
+		return importo;
+	}
+
+	public void setImporto(double importo) {
+		this.importo = importo;
+	}
+
+	@Override
+	public String toString() {
+		return "Scontrino {\ncodiceScontrino=" + codiceScontrino + "\ncliente=" + cliente + "\nlista=" + lista
+				+ "\nimporto=" + importo + "}";
+	}
+
+}
