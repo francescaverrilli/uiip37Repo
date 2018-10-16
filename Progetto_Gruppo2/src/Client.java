c5da788eb494c6bb52356fdd479e50e44e256451
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-16 15:24:28 +0200

    metodo daniela

--- Progetto_Gruppo2/src/Client.java
+++ Progetto_Gruppo2/src/Client.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
 
 public class Client {
 
 	public static void main(String[] args) {
-
+		LinkedList<Persona> elencoPersone = new LinkedList<Persona>();
+		
+		
+		
 		Indirizzo ind = new Indirizzo(10, "via bari", 28, 72013, "Ceglie Messapica", "br", "puglia");
 		Persona p = new Persona("sergio", "santoro", "21/12/1991", ind);
 		System.out.println(p.getNome());
@@ -11,6 +15,7 @@ public class Client {
 		System.out.println(p.getIndirizzo().getVia());
 		System.out.println(p.getIndirizzo().getNumerocivvico());
 		
+		elencoPersone.add(p);
 		Indirizzo i_Daniela = new Indirizzo(10, "Via Monticchio", 4, 83031, "Ariano Irpino", "AV", "Campania");
 		Persona p_Daniela = new Persona("Daniela", "Guardabascio", "19 settembre 1985", i_Daniela);
 		System.out.println("Dati Anagrafici");
@@ -21,7 +26,7 @@ public class Client {
 		System.out.println(p_Daniela.getIndirizzo().getVia() + " " + p_Daniela.getIndirizzo().getNumerocivvico() + " "
 				+ p_Daniela.getIndirizzo().getCitta());
 		
-
+		elencoPersone.add(p_Daniela);
 		Indirizzo i = new Indirizzo(34, "Via Iscalonga", 70, 83036, "Mirabella Eclano", "AV", "Campania");
 		Persona Angela = new Persona("Angela", "Di Pietro", "11/06/1992", i);
 		
@@ -34,7 +39,7 @@ public class Client {
 		System.out.println(Angela.getIndirizzo().getVia() + " " + Angela.getIndirizzo().getNumerocivvico() + " "
 				+ Angela.getIndirizzo().getCitta());
 		
-		
+		elencoPersone.add(Angela);
 		Indirizzo indirizzo = new Indirizzo (12, "Contrada San Cesareo", 30, 83030, "Zungoli", "AV", "Campania");
 		Persona Chiara = new Persona ("Chiara", "Annicchiarico", "16/04/90", indirizzo);
 		System.out.println("Dati Anagrafici");
@@ -46,6 +51,14 @@ public class Client {
 				+ Chiara.getIndirizzo().getCitta());
 
 
+		elencoPersone.add(Chiara);
+		
+		ResidentiCampaniaGuardabascio.residentiCampania(elencoPersone);
+		System.out.println("Elenco persone residenti in campania");
+		for( Persona p_d : elencoPersone) {
+			System.out.println(p_d.getNome());
+		}
+		
 	}
 
 }
