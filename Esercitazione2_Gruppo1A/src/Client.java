ea1edcf01428e5fd480aef3b0253507587326275
francescoabbatangelo <francescoabbatangelo91@gmail.com>
2018-10-16 18:17:35 +0200

    Frank

--- Esercitazione2_Gruppo1A/src/Client.java
+++ Esercitazione2_Gruppo1A/src/Client.java
@@ -2,8 +2,10 @@
 public class Client {
 
 	public static void main(String[] args) {
-		
+		Cliente Frank = new Cliente("001", "Frank", "Abba", 100);
 
+		Prodotto patatine = new Prodotto("P1", "Patatine", 1, 2);
+		Prodotto pizza = new Prodotto("P2", "Pizza", 2, 4);
 	}
 
 }
