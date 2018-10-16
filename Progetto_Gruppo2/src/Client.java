c89765224c03704a624fe351f98e769a6bd60e66
AngelaDiPietro <angeladipietro92@gmail.com>
2018-10-16 15:02:07 +0200

    Angela

--- Progetto_Gruppo2/src/Client.java
+++ Progetto_Gruppo2/src/Client.java
@@ -5,16 +5,14 @@ public class Client {
 
 		Indirizzo ind = new Indirizzo(10, "via bari", 28, 72013, "Ceglie Messapica", "br", "puglia");
 		Persona p = new Persona("sergio", "santoro", "21/12/1991", ind);
-
-		Indirizzo i_Daniela = new Indirizzo(10, "Via Monticchio", 4, 83031, "Ariano Irpino", "AV", "Campania");
-		Persona p_Daniela = new Persona("Daniela", "Guardabascio", "19 settembre 1985", i_Daniela);
-
 		System.out.println(p.getNome());
 		System.out.println(p.getCognome());
 		System.out.println(p.getDataNascita());
 		System.out.println(p.getIndirizzo().getVia());
 		System.out.println(p.getIndirizzo().getNumerocivvico());
-
+		
+		Indirizzo i_Daniela = new Indirizzo(10, "Via Monticchio", 4, 83031, "Ariano Irpino", "AV", "Campania");
+		Persona p_Daniela = new Persona("Daniela", "Guardabascio", "19 settembre 1985", i_Daniela);
 		System.out.println("Dati Anagrafici");
 		System.out.println(p_Daniela.getNome());
 		System.out.println(p_Daniela.getCognome());
@@ -26,12 +24,14 @@ public class Client {
 
 		Indirizzo i = new Indirizzo(34, "Via Iscalonga", 70, 83036, "Mirabella Eclano", "AV", "Campania");
 		Persona Angela = new Persona("Angela", "Di Pietro", "11/06/1992", i);
-
-		System.out.println("Il nome della Persona è " + Angela.getNome());
-		System.out.println("Il cognome" + Angela.getCognome());
-		System.out.println("Nata il " + Angela.getDataNascita());
-		System.out.println("Residente a" + i.getCitta() + i.getNumerocivvico() + i.getProvincia() + i.getRegione());
-
+		System.out.println(" ");
+		System.out.println("Dati Anagrafici");
+		System.out.println(Angela.getNome());
+		System.out.println(Angela.getCognome());
+		System.out.println(Angela.getDataNascita());
+		System.out.println("Indirizzo:");
+		System.out.println(Angela.getIndirizzo().getVia() + " " + Angela.getIndirizzo().getNumerocivvico() + " "
+				+ Angela.getIndirizzo().getCitta());
 	}
 
 }
