463e0f13bf43386fdb8fc43614784c4b7468f68f
AndreaGuastaferro <andrea.guastaferro91@gmail.com>
2018-10-20 22:27:36 +0200

    Homework4_Andrea_Guastaferro_w1We

---
+++ Homework4_Guastaferro/src/main/java/homework4/Guastaferro/model/Telefono.java
@@ -0,0 +1,38 @@
+package homework4.Guastaferro.model;
+
+public class Telefono {
+
+	private String numero;
+	private int candidato;
+	
+	public Telefono(String numero, int candidato) {
+		
+		this.numero=numero;
+		this.candidato=candidato;
+		
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public int getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(int candidato) {
+		this.candidato = candidato;
+	}
+
+	@Override
+	public String toString() {
+		return "Telefono [numero=" + numero + ", candidato=" + candidato + "]";
+	}
+	
+	
+
+}
